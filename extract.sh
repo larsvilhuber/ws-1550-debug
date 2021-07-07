05a77e7ef04c971e96b11e108f954d751eafe9c2
Lars Vilhuber <lars@vilhuber.com>
2021-07-07 10:50:42 -0400

    Technical updates

--- extract.sh
+++ extract.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-grep "Outdoor1ID" * | awk ' { print $1, $9 } ' | sed 's/meteo-//; s/\.html//; s/_/,/; s/: /,/; s/value=//'
+echo "date,time,value"
+grep "Outdoor1ID" meteo*html | awk ' { print $1, $9 } ' | sed 's/meteo-//; s/\.html//; s/_/,/; s/: /,/; s/value=//'
