0d9b7cc800fc5a9ceee0d595f3b5a342270bd855
Lars Vilhuber <lars@vilhuber.com>
2021-08-07 23:53:45 -0400

    Minor path change

--- monitor.sh
+++ monitor.sh
@@ -2,9 +2,10 @@
 
 URL=http://192.168.1.248/livedata.htm
 PREFIX=meteo
+MYPATH=$(dirname $0)
 DELAY=300
 
-OUTFILE=$HOME/meteo/${PREFIX}-$(date +%F_%H-%M).html
+OUTFILE=$MYPATH/${PREFIX}-$(date +%F_%H-%M).html
 wget --quiet -O $OUTFILE $URL
  
 
